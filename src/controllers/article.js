5288849f80b03c899ac6d8267fdaee29c70fb8ac
alex-kon-newsuk <alexandros.konidaris@news.co.uk>
2016-01-12 13:40:37 +0000

    Finish the basik skeleton template - make a request and contact CAPI and dispaly the response

--- src/controllers/article.js
+++ src/controllers/article.js
@@ -1,3 +1,15 @@
+var requestService = require('../services/requestService');
+
 module.exports.get = function (request, response, next) {
-    response.send('sucess');
+    requestService.get(request, response, next).then(parseRequest);
+ };
+
+function parseRequest (data) {
+    var processedData = processResponseData (data.body);
+    data.response.setHeader('Content-Type', 'application/json');
+    data.response.send(data);
+}
+
+function processResponseData (data) {
+    return data;
 }
---
+++ src/services/requestService.js
@@ -0,0 +1,25 @@
+var requestModule = require('request');
+var Promise = require('promise');
+var config = require('config');
+
+var basepath = config.get('CAPI.basepath');
+var version = config.get('CAPI.version');
+var apikey = config.get('CAPI.apikey');
+
+module.exports.get = function (request, response, next, callback) {
+
+    return new Promise(function (resolve, reject) {
+
+        requestModule(basepath + '/content/' + version + '/' + request.params.id +
+                      '?format=json&includeRelated=true&html=full%2Cbody&api_key=' + apikey,
+                        function (error, httpResponse, body) {
+
+            resolve({
+                body : body,
+                response : response
+            });
+
+        });
+    });
+
+}
